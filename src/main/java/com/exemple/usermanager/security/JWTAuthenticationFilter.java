14c3c25f4510a3a581ebb60b7455216a9a530bbd
Djibril Ba <djibril15.ba@ucad.edu.sn>
2023-10-09 14:12:45 +0000

    Ajout de la classe JWTAuthenticationFilter

---
+++ src/main/java/com/exemple/usermanager/security/JWTAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package com.exemple.usermanager.security;
+
+import com.exemple.usermanager.entities.User;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.json.JsonParseException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super();
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        User user = null;
+
+        try {
+            user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        }
+
+        return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+    }
+}
